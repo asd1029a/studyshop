0103afc1c274e5e3bd4bea7f0ed6510bafd56a04
kim <kimth5533@naver.com>
2022-08-31 18:05:35 +0900

    day2

---
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,79 @@
+package com.shop.controller;
+
+import com.shop.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping("/ex01")
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "타임리프 예제 입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDTO = new ItemDto();
+        itemDTO.setItemDetail("상품 상세 설명");
+        itemDTO.setItemNm("테스트 상품1");
+        itemDTO.setPrice(10000);
+        itemDTO.setRegTime(LocalDateTime.now());
+        model.addAttribute("itemDto", itemDTO);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model) {
+        ArrayList<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=1;i<=10;i++) {
+            ItemDto itemDto1 = new ItemDto();
+            itemDto1.setItemDetail("상품 상세 설명"+i);
+            itemDto1.setItemNm("테스트 상품"+i);
+            itemDto1.setPrice(10000*i);
+            itemDto1.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto1);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+
+    @GetMapping("/ex04")
+    public String thymeleafExample04(Model model) {
+        ArrayList<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=1;i<=10;i++) {
+            ItemDto itemDto1 = new ItemDto();
+            itemDto1.setItemDetail("상품 상세 설명"+i);
+            itemDto1.setItemNm("테스트 상품"+i);
+            itemDto1.setPrice(10000*i);
+            itemDto1.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto1);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
+    @GetMapping("ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping("ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+}
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -6,28 +6,25 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import com.shop.entity.QItem;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.TestPropertySource;
 import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
-    @Autowired
+
+    @PersistenceContext
     EntityManager em;
 
     @Test
